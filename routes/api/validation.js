eace36391ca1e1a0f0c92ac28ddbc78662c70ae3
Powerman-code <64640573+Powerman-code@users.noreply.github.com>
2021-05-23 15:11:18 +0300

    Start

---
+++ routes/api/validation.js
@@ -0,0 +1,46 @@
+const Joi = require("joi");
+
+const schemaCreateContact = Joi.object({
+  name: Joi.string().alphanum().min(1).max(30).required(),
+  email: Joi.string().min(3).max(45).required(),
+  phone: Joi.string().min(10).max(30).required(),
+  favorite: Joi.boolean().required(),
+});
+
+const schemaUpdateContact = Joi.object({
+  name: Joi.string().alphanum().min(1).max(30).optional(),
+  email: Joi.string().alphanum().min(3).max(45).optional(),
+  phone: Joi.string().min(10).max(30).optional(),
+  favorite: Joi.boolean().optional(),
+});
+
+const validate = (schema, obj, next) => {
+  const { error } = schema.validate(obj);
+  if (error) {
+    const [{ message }] = error.details;
+    return next({
+      status: 400,
+      message: `Field: ${message.replace(/"/g, "")} `,
+    });
+  }
+  next();
+};
+
+module.exports.createContact = (req, _res, next) => {
+  return validate(schemaCreateContact, req.body, next);
+};
+
+module.exports.updateContact = (req, _res, next) => {
+  return validate(schemaUpdateContact, req.body, next);
+};
+
+module.exports.queryMongoIdValid = (id) => (req, res, next) => {
+  const isValid = mongoose.Types.ObjectId.isValid(req.params[id]);
+  if (isValid) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: `Params is not valid ObjectId`,
+  });
+};
