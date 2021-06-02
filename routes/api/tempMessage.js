a0ce65b00b7929c30fabfdb0a1058d9717eab87e
Powerman-code <64640573+Powerman-code@users.noreply.github.com>
2021-06-02 21:03:43 +0300

    nurned off validation of tempMessage

--- routes/api/tempMessage.js
+++ routes/api/tempMessage.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const tempMessageController = require("../../controllers/tempMessageController");
-const validate = require("./validationTempMessage");
+// const validate = require("./validationTempMessage");
 
 router
   .get("/", tempMessageController.getMessage)
@@ -10,5 +10,9 @@ router
   //     // validate.createTempMessage,
   //     tempMessageController.create)
 
-  .patch("/", validate.updateTempMessage, tempMessageController.updateMessage);
+  .patch(
+    "/",
+    // validate.updateTempMessage,
+    tempMessageController.updateMessage
+  );
 module.exports = router;
